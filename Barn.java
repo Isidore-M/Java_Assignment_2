59b0ef70c5454665eb07bde0d6ef979a3ddc3805
Isidore-M <isidore.mbargamanga@triosstudent.com>
2025-07-25 04:17:20 -0400

    Added the total of milk per day and duck's name

--- Barn.java
+++ Barn.java
@@ -9,16 +9,21 @@ public class Barn {
   private static Scanner Sinput = new Scanner(System.in);
   private static ArrayList<Goat> goats = new ArrayList<Goat>();
   private static ArrayList<Duck> ducks = new ArrayList<Duck>();
+  private static int mpd = input.nextInt();
+  private static int total;
+  
 
   public static void main(String[] args) {
     System.out.println("\n--------Welcome to you Barn genarator------\n");
     System.out.print("How many animals in the barn? ");
     int numAnimal = input.nextInt();
+    
 
     for(int i = 0; i < numAnimal; i++) {
      
       System.out.printf("%nEnter animal #%d type (goat / duck) ", i+1 );
       String type = Sinput.nextLine();
+      
       
       if(type.equals("goat") ) { 
         Goat goat = new Goat("Goat"); 
@@ -26,6 +31,8 @@ public class Barn {
         System.out.print("What is the Milk per day? : ");
         int mpd = input.nextInt();
         goat.setmilkPerDay(mpd);
+        total+=mpd; 
+        
       
       }
 
@@ -35,6 +42,10 @@ public class Barn {
         System.out.print("What is the egg colour? : ");
         String colour = Sinput.nextLine();
         duck.seteggColor(colour);
+
+        System.out.print("What is the Name of that Duck? : ");
+        String dName = Sinput.nextLine();
+        duck.setduckName(dName);
       }
 
     } 
@@ -43,11 +54,12 @@ public class Barn {
       System.out.printf("Type: %s Milk per day: %dL %n",
       goats.get(i).getType(), goats.get(i).getmilkPerDay());
     } 
+      System.out.printf("---%n  Total milk per day is: %dL   ---", total);
     System.out.print("\n---------- All ducks ----------\n\n");
 
     for(int i = 0; i < ducks.size(); i++) {
-      System.out.printf("Type: %s, Egg Color is : %s%n",
-        ducks.get(i).getType(), ducks.get(i).geteggColor());
+      System.out.printf("Type: %s, Egg Color is : %s and Name is:  %s%n",
+        ducks.get(i).getType(), ducks.get(i).geteggColor(), ducks.get(i).getduckName());
     }
     System.out.println("\n-------- See you later in the Barn ------\n");
     input.close();
--- README.md
+++
@@ -1,2 +0,0 @@
-# Java_Assignment_2
-Second week of Java.... Assignment two
